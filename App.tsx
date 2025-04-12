12e69a36f9627a0660ab2811b1cf0c4b37327cad
whiony <whiony@gmail.com>
2025-04-12 21:19:23 +0200

    Add new font

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  SplashScreen.hideAsync();
   return <AppNavigator />;
 }
